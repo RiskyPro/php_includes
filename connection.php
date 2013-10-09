5ebf173052a4ad586bd75f9b0357334ef08f4023
RiskyPro <hs_dahneem@hotmail.com>
2013-10-09 13:10:50 +0300

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php require_once("constants.php");?>
+<?php
+	$connection = mysql_connect(DB_HOST,DB_USER,DB_PASS);
+	if(!$connection)
+	{	die("Database connection failed " . mysql_error()); }
+	$db_select = mysql_select_db(DB_NAME,$connection);
+	if(!$db_select)
+	{ die("Database selection failed " . mysql_error()); }
+
+?>
